735976b25e7dcb32de6b3b9ae37c42a32c4d8208
Laveesha kumra <37627570+LaveeshaKumra@users.noreply.github.com>
2019-07-29 13:38:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/",function(req,res){
 app.post('/ss', function(req, res) {
 	 console.log(req.body.num);
   var accountSid = 'AC84511a70e616f9f0c67545f8ce61d85d'; 
-  // Your Account SID from www.twilio.com/console password:laveeshakumra123
+  // Your Account SID from www.twilio.com/console 
   
 var authToken = 'f933f85129a8defe22d3ceab00f5cf52';   
 // Your Auth Token from www.twilio.com/console
@@ -30,7 +30,7 @@ var client = new twilio(accountSid, authToken);
 
 client.messages.create({
     body: 'Hello how are you?',
-    to: 'XXXXXXXXXXXX',  // Text this number
+    to: 'XXXXXXXXXXXX',  // Text this number // THIS NUMBER MUST BE VERIFIED FROM YOUR TWILIO ACCOUNT 
     from: 'XXXXXXXXXX' // From a valid Twilio number
 })
 .then((message) => console.log(message.sid));
